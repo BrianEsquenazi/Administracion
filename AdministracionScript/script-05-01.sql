010d7ae639249f957e53288151f3d5d4ff7c9ac5
brianesquenazi <brianesquenazi@yahoo.com.ar>
2016-05-03 18:23:11 -0300

    Corrijo script procedure

--- AdministracionScript/script-05-01.sql
+++ AdministracionScript/script-05-01.sql
@@ -9,7 +9,38 @@ ALTER TABLE	sufractanSA.dbo.Cuenta
 	ADD PRIMARY KEY (Cuenta)
 GO
 
-CREATE PROCEDURE modidicar_cuenta
+/*
+	ELIMINACION DE PROCEDIMIENTOS Y FUNCIONES
+*/
+
+USE [sufractanSA]
+GO
+
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PR_alta_cuenta]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[PR_alta_cuenta]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PR_modificar_cuenta]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[PR_modificar_cuenta]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PR_baja_cuenta]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[PR_baja_cuenta]
+GO
+
+/*
+	CREACION DE PROCEDIMIENTOS Y FUNCIONES
+*/
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER OFF
+GO
+
+	
+CREATE PROCEDURE PR_modificar_cuenta
 	(@cuenta varchar(10),
 	@descripcion varchar(50),
 	@nivel smallint,
@@ -35,7 +66,7 @@ AS
 	END CATCH
 GO
 	
-CREATE PROCEDURE alta_cuenta
+CREATE PROCEDURE PR_alta_cuenta
 	(@cuenta varchar(10),
 	@descripcion varchar(50),
 	@nivel smallint,
@@ -52,13 +83,13 @@ AS
 	BEGIN CATCH	
 		ROLLBACK TRANSACTION
 		IF (@mensaje_error = '') 
-				EXEC modificar_cuenta @cuenta, @descripcion, @nivel, @empresa	
+				EXEC PR_modificar_cuenta @cuenta, @descripcion, @nivel, @empresa	
 		RAISERROR(@mensaje_error, 16, 217)
 			WITH SETERROR
 	END CATCH
 GO	
 
-CREATE PROCEDURE baja_cuenta
+CREATE PROCEDURE PR_baja_cuenta
 	(@cuenta varchar(10))
 AS
 	DECLARE @mensaje_error varchar(255)
