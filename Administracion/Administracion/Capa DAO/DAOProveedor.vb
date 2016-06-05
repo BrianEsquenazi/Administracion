1fafdca407af0aa93f7f54a59cac0d2013b4f5bf
MartinGotelli <martingotelliferenaz@gmail.com>
2016-06-04 21:56:54 -0300

    Primer versión de los controls, se ven chiquitos

---
+++ Administracion/Administracion/Capa DAO/DAOProveedor.vb
@@ -0,0 +1,14 @@
+﻿Imports ClasesCompartidas
+
+Public Class DAOProveedor
+
+    Public Function listado_provincias() As List(Of Provincia)
+        Dim provincias As New List(Of Provincia)
+        For Each row In SQLConnector.retrieveDataTable("listado_provincias").Rows
+            provincias.Add(New Provincia(row("codigo"), row("nombre")))
+        Next
+        Return provincias
+    End Function
+
+
+End Class
