d9427a3d7b2e3c5dfc47a4082a7254e6822ce7c8
MartinGotelli <martingotelliferenaz@gmail.com>
2016-06-24 17:23:29 -0300

    Agregande pause al ejecutar.bat

--- AdministracionScript/ejecutar.bat
+++ AdministracionScript/ejecutar.bat
@@ -6,3 +6,4 @@ sqlcmd -i ScriptProcedures.sql -U usuarioadmin -P usuarioadmin -S localhost\sqls
 sqlcmd -i ScriptFunciones.sql -U usuarioadmin -P usuarioadmin -S localhost\sqlserver2008
 sqlcmd -i ScriptProcesos.sql -U usuarioadmin -P usuarioadmin -S localhost\sqlserver2008
 sqlcmd -i ScriptNovedades.sql -U usuarioadmin -P usuarioadmin -S localhost\sqlserver2008
+pause
