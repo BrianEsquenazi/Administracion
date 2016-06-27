29f6417fd92b3f0ffe3a60dc7ef4e04ebb644b58
brianesquenazi <brianesquenazi@yahoo.com.ar>
2016-06-27 20:45:59 -0300

    Cambio en script novedades

---
+++ Administracion/Administracion/Capa DAO/DAOCtaCteProveedor.vb
@@ -0,0 +1,12 @@
+﻿Imports ClasesCompartidas
+
+Public Class DAOCtaCteProveedor
+
+    Public Shared Function buscarCuentas() As List(Of CtaCteProveedor)
+        Dim cuentas As New List(Of CtaCteProveedor)
+        For Each row In SQLConnector.retrieveDataTable("get_carga_intereses").Rows
+            cuentas.Add(New CtaCteProveedor(row("FechaOriginal").ToString, row("DesProveOriginal").ToString, row("FacturaOriginal").ToString, row("Cuota").ToString, row("fecha").ToString, row("Saldo"), row("Intereses"), row("IvaIntereses"), row("Referencia").ToString, row("Cuota").ToString, row("NroInterno").ToString))
+        Next
+        Return cuentas
+    End Function
+End Class
--- Administracion/Administracion/CargaIntereses.Designer.vb
+++ Administracion/Administracion/CargaIntereses.Designer.vb
@@ -23,30 +23,62 @@ Partial Class CargaIntereses
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.gridCtaCte = New System.Windows.Forms.DataGridView()
+        Me.btnCancela = New WindowsApplication1.CustomButton()
+        Me.btnGraba = New WindowsApplication1.CustomButton()
         CType(Me.gridCtaCte, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'gridCtaCte
         '
         Me.gridCtaCte.AllowUserToAddRows = False
+        Me.gridCtaCte.AllowUserToDeleteRows = False
+        Me.gridCtaCte.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader
         Me.gridCtaCte.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.gridCtaCte.Location = New System.Drawing.Point(12, 12)
         Me.gridCtaCte.Name = "gridCtaCte"
-        Me.gridCtaCte.Size = New System.Drawing.Size(760, 350)
+        Me.gridCtaCte.Size = New System.Drawing.Size(760, 468)
         Me.gridCtaCte.StandardTab = True
         Me.gridCtaCte.TabIndex = 1
+        '
+        'btnCancela
+        '
+        Me.btnCancela.Cleanable = False
+        Me.btnCancela.EnterIndex = -1
+        Me.btnCancela.LabelAssociationKey = -1
+        Me.btnCancela.Location = New System.Drawing.Point(385, 496)
+        Me.btnCancela.Name = "btnCancela"
+        Me.btnCancela.Size = New System.Drawing.Size(122, 42)
+        Me.btnCancela.TabIndex = 3
+        Me.btnCancela.Text = "Cancela"
+        Me.btnCancela.UseVisualStyleBackColor = True
+        '
+        'btnGraba
+        '
+        Me.btnGraba.Cleanable = False
+        Me.btnGraba.EnterIndex = -1
+        Me.btnGraba.LabelAssociationKey = -1
+        Me.btnGraba.Location = New System.Drawing.Point(248, 496)
+        Me.btnGraba.Name = "btnGraba"
+        Me.btnGraba.Size = New System.Drawing.Size(122, 42)
+        Me.btnGraba.TabIndex = 2
+        Me.btnGraba.Text = "Graba"
+        Me.btnGraba.UseVisualStyleBackColor = True
         '
         'CargaIntereses
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(784, 562)
+        Me.Controls.Add(Me.btnCancela)
+        Me.Controls.Add(Me.btnGraba)
         Me.Controls.Add(Me.gridCtaCte)
         Me.Name = "CargaIntereses"
-        Me.Text = "Carga Intereses"
+        Me.Text = "Actualizacion de Deuda de Pyme Nacion"
         CType(Me.gridCtaCte, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents gridCtaCte As System.Windows.Forms.DataGridView
+    Friend WithEvents btnGraba As WindowsApplication1.CustomButton
+    Friend WithEvents btnCancela As WindowsApplication1.CustomButton
 End Class
