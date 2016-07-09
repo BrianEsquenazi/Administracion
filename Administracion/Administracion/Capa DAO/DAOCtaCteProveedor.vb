94d56d3dd2f6946b43c65f4fad012d9651965ba6
brianesquenazi <brianesquenazi@yahoo.com.ar>
2016-07-09 13:07:16 -0300

    Alta en base de compras con PyME Nación

--- Administracion/Administracion/Capa DAO/DAOCtaCteProveedor.vb
+++ Administracion/Administracion/Capa DAO/DAOCtaCteProveedor.vb
@@ -35,12 +35,12 @@ Public Class DAOCtaCteProveedor
     End Function
 
 
-    Public Shared Function buscardeuda(ByVal proveedor As String)
-        Dim ctacteprv As New List(Of CtaCteProveedoresDeuda)
-        For Each row In SQLConnector.retrieveDataTable("buscar_Cuenta_Corriente_Proveedores_deuda", proveedor).Rows
-            ctacteprv.Add(New CtaCteProveedoresDeuda(row("Tipo").ToString, row("letra").ToString, row("punto").ToString, row("numero").ToString, row("total"), row("saldo"), row("fecha").ToString, row("vencimiento").ToString))
-        Next
-        Return ctacteprv
-    End Function
+    'Public Shared Function buscardeuda(ByVal proveedor As String)
+    '    Dim ctacteprv As New List(Of CtaCteProveedoresDeuda)
+    '    For Each row In SQLConnector.retrieveDataTable("buscar_Cuenta_Corriente_Proveedores_deuda", proveedor).Rows
+    '        ctacteprv.Add(New CtaCteProveedoresDeuda(row("Tipo").ToString, row("letra").ToString, row("punto").ToString, row("numero").ToString, row("total"), row("saldo"), row("fecha").ToString, row("vencimiento").ToString))
+    '    Next
+    '    Return ctacteprv
+    'End Function
 
 End Class
